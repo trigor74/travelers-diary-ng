f892d60c545fdb68aea8c11e550cf2ca2c4b7d8b
Igor Trifonov <trigor74@gmail.com>
2017-06-12 10:16:47 +0300

    toolbar, sidenav, theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import {MaterialModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/firebaseconfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAoIiPF4bYZKSpBXf0kOKVVfycO4VNQSec',
+    authDomain: 'resplendent-torch-7243.firebaseapp.com',
+    databaseURL: 'https://resplendent-torch-7243.firebaseio.com',
+    projectId: 'resplendent-torch-7243',
+    storageBucket: 'resplendent-torch-7243.appspot.com',
+    messagingSenderId: '1079124304601'
+};

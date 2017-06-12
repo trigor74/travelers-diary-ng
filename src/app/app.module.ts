b70bd615ae8e8f46d4abe9065af5b25891228314
Igor Trifonov <trigor74@gmail.com>
2017-06-12 12:32:18 +0300

    created components home, account

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
-import {MaterialModule} from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AccountComponent
   ],
   imports: [
     BrowserAnimationsModule,

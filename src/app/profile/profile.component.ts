0caa546f3ce93fca8ed20de463eb276e2ab935a7
Igor Trifonov <trigor74@gmail.com>
2017-06-14 00:37:00 +0300

    change profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  uid = '';
+  name = '';
+  email = '';
+  photoURL = '';
+  edit = false;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.uid = this.authService.uid;
+    this.name = this.authService.userInfo.displayName;
+    this.email = this.authService.userInfo.email;
+    this.photoURL = this.authService.userInfo.photoURL;
+  }
+
+  editMode(isEditable: boolean) {
+    this.edit = isEditable;
   }
 
 }

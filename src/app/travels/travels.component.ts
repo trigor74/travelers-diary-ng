0b858824024153c515538c19f4e58371071d185c
Igor Trifonov <trigor74@gmail.com>
2017-06-14 16:09:47 +0300

    added travel list

--- src/app/travels/travels.component.ts
+++ src/app/travels/travels.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-travels',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./travels.component.css']
 })
 export class TravelsComponent implements OnInit {
+  items: FirebaseListObservable<any>;
+  uid = '';
 
-  constructor() { }
+  constructor(db: AngularFireDatabase, private authService: AuthService) {
+    this.uid = this.authService.uid;
+    this.items = db.list('/users/' + this.uid + '/travels', {
+      query: {
+        orderByChild: 'creationTime',
+        startAt: 0
+      }
+    });
+  }
 
   ngOnInit() {
   }
 
+  edit(item: any) {
+
+  }
+
 }
+
+// class Travel {
+//   creationTime: number;
+//   start: number;
+//   stop: number;
+//   title: string;
+//   description: string;
+//   active: boolean;
+//   picasaAlbumId: string;
+//   defaultCover: string;
+//   userCover: string;
+// }
+
+// https://resplendent-torch-7243.firebaseio.com/users/:uid/travels
+// query = mFirebaseRef.orderByChild("creationTime ").startAt(0);
